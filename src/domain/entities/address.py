1dee25d6f2376dc04e8b1fcfb3237b0a9090b5d9
moises monteiro <moises.monteiro2077@hotmail.com>
2023-11-04 17:57:10 -0300

    update entities

--- src/domain/entities/address.py
+++ src/domain/entities/address.py
@@ -1,22 +1,31 @@
-from dataclasses import dataclass
-
-
 class Constraint:
     def __init__(
         self,
         id: int,
         user_id: int,
     ) -> None:
-        self.id: id
-        self.user_id: user_id
+        self.id = id
+        self.user_id = user_id
 
 
-@dataclass
 class Address(Constraint):
-    postal_code: str
-    uf: str
-    city: str
-    neighborhood: str
-    street: str
-    number: str
-    complement: str
+    def __init__(
+        self,
+        id: int,
+        user_id: int,
+        postal_code: str,
+        uf: str,
+        city: str,
+        neighborhood: str,
+        street: str,
+        number: str,
+        complement: str,
+    ) -> None:
+        super().__init__(id, user_id)
+        self.postal_code = postal_code
+        self.uf = uf
+        self.city = city
+        self.neighborhood = neighborhood
+        self.street = street
+        self.number = number
+        self.complement = complement
