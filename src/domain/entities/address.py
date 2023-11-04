f83ee836a0cc8158ba099e908491301a3118af38
moises monteiro <moises.monteiro2077@hotmail.com>
2023-11-04 17:52:29 -0300

    update entities

--- src/domain/entities/address.py
+++ src/domain/entities/address.py
@@ -1,10 +1,14 @@
 from dataclasses import dataclass
 
 
-@dataclass
 class Constraint:
-    id: int
-    user_id: int
+    def __init__(
+        self,
+        id: int,
+        user_id: int,
+    ) -> None:
+        self.id: id
+        self.user_id: user_id
 
 
 @dataclass
