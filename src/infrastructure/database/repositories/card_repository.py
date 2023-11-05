ec1cddea06b50afcdc829a17f25a7e49878ea402
moises monteiro <moises.monteiro2077@hotmail.com>
2023-11-05 04:40:51 -0300

    Update card_repository

---
+++ src/infrastructure/database/repositories/card_repository.py
@@ -0,0 +1,66 @@
+from typing import Any
+
+from sqlalchemy.orm import Session
+
+from src.data.repositories.card_repository import CardRepositoryInterface
+from src.domain.entities.card import Card
+from src.infrastructure.database.models.card import Card as CardModel
+
+
+class CardRepository(CardRepositoryInterface):
+    def __init__(self, session: Session):
+        self.session: Session = session
+
+    def list_card(self) -> list[Card] | None:
+        try:
+            return self.session.query(CardModel).all()
+        except:
+            return None
+
+    def get_card(self, id: int) -> Card | None:
+        try:
+            return (
+                self.session.query(CardModel).filter(CardModel.id == id).one_or_none()
+            )
+        except:
+            return None
+
+    def create_card(self, card: Card) -> Card | None:
+        try:
+            card_data = {
+                "user_id": card.user_id,
+                "name": card.name,
+                "number": card.number,
+                "expiration_month": card.expiration_month,
+                "expiration_year": card.expiration_year,
+                "cvc": card.cvc,
+            }
+
+            card_model = CardModel(**card_data)
+
+            self.session.add(card_model)
+            self.session.commit()
+            return card_model
+
+        except:
+            return None
+
+    def update_card(self, id: int, update_fields: dict[str, Any]) -> Card | None:
+        try:
+            self.session.query(CardModel).filter(CardModel.id == id).update(
+                update_fields
+            )
+            self.session.commit()
+            card_updated = self.get_card(id)
+
+            return card_updated
+        except:
+            return None
+
+    def delete_card(self, id: int) -> bool:
+        try:
+            self.session.query(CardModel).filter(CardModel.id == id).delete()
+            self.session.commit()
+            return True
+        except:
+            return False
