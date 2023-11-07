c152d84368809b32c970bedf4f094723f41b26fb
moises monteiro <moises.monteiro2077@hotmail.com>
2023-11-07 10:10:48 -0300

    Updated router

--- src/presentation/routers/card_router.py
+++ src/presentation/routers/card_router.py
@@ -8,7 +8,8 @@ from src.presentation.composers.create_card_composer import (
 from src.presentation.composers.delete_card_composer import (
     delete_card_composer,
 )
-from src.presentation.composers.list_cards_composer import list_cards_composer
+from src.presentation.composers.get_card_composer import get_card_composer
+from src.presentation.composers.list_card_composer import list_card_composer
 from src.presentation.composers.update_card_composer import (
     update_card_composer,
 )
@@ -17,32 +18,34 @@ from src.presentation.schemas.card import CardCreate, CardOut, CardUpdate
 router = APIRouter(prefix="/cards", tags=["cards"])
 
 
-@router.get("/", status_code=status.HTTP_200_OK, response_model=list[CardOut])
-def list_cards(session: Session = Depends(get_db)):
-    cards = list_cards_composer(session)
+@router.get(
+    "/list/{user_id}", status_code=status.HTTP_200_OK, response_model=list[CardOut]
+)
+def list_cards(user_id: int, session: Session = Depends(get_db)):
+    cards = list_card_composer(session, user_id)
     if cards:
-        return [cards]
+        return cards
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND, detail="List cards not found."
     )
 
 
-@router.get("/me/{id}", status_code=status.HTTP_200_OK, response_model=CardOut)
+@router.get("/{id}", status_code=status.HTTP_200_OK, response_model=CardOut)
 def get_card(
     id: int,
     session: Session = Depends(get_db),
 ):
-    card = get_card(id, session)
-    if card:
-        return card
+    address = get_card_composer(session, id)
+    if address:
+        return address
     raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND, detail="get-card for id, not found."
+        status_code=status.HTTP_404_NOT_FOUND, detail="get-address for id, not found."
     )
 
 
-@router.post("/", status_code=status.HTTP_201_CREATED, response_model=CardOut)
-def create_card(card: CardCreate, session: Session = Depends(get_db)):
-    card = create_card_composer(session, card)
+@router.post("/{user_id}", status_code=status.HTTP_201_CREATED, response_model=CardOut)
+def create_card(user_id: int, card: CardCreate, session: Session = Depends(get_db)):
+    card = create_card_composer(session, card, user_id)
 
     if card:
         return card
