0f72c7138b39fdfdfc2269a9eddb2a130f179029
moises monteiro <moises.monteiro2077@hotmail.com>
2023-11-07 14:15:34 -0300

    Update models

--- src/infrastructure/database/models/address.py
+++ src/infrastructure/database/models/address.py
@@ -1,24 +1,23 @@
 from sqlalchemy import ForeignKey, String
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.orm import Mapped, backref, mapped_column, relationship
 
 from src.infrastructure.database.models.base import Base
-from src.infrastructure.database.models.user import User
 
 
 class Address(Base):
     __tablename__ = "address"
 
-    id: Mapped[int] = mapped_column(primary_key=True, unique=True)
+    id: Mapped[int] = mapped_column("id", primary_key=True, unique=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
-    postal_code: Mapped[str] = mapped_column(
-        "Postal Code", String(length=8), nullable=False
+    postal_code: Mapped[str] = mapped_column(String(length=8))
+    uf: Mapped[str] = mapped_column(String(length=2))
+    city: Mapped[str] = mapped_column(String(length=150))
+    neighborhood: Mapped[str] = mapped_column(String(length=150))
+    street: Mapped[str] = mapped_column(String(length=150))
+    number: Mapped[str] = mapped_column(String(length=20))
+    complement: Mapped[str] = mapped_column(String(length=150))
+    user = relationship(
+        "User",
+        backref=backref("addresses", uselist=True),
+        foreign_keys=[user_id],
     )
-    uf: Mapped[str] = mapped_column("UF", String(length=2), nullable=False)
-    city: Mapped[str] = mapped_column("City", String(length=150), nullable=False)
-    neighborhood: Mapped[str] = mapped_column(
-        "Neighborhood", String(length=150), nullable=False
-    )
-    street: Mapped[str] = mapped_column("Street", String(length=150), nullable=False)
-    number: Mapped[str] = mapped_column("Number", String(length=20), nullable=False)
-    complement: Mapped[str] = mapped_column("Complement", String(length=150))
-    user: Mapped["User"] = relationship(back_populates="address", uselist=False)
